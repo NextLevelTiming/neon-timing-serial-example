7b64f2aea9275bbf714e8ad931b3a0a2d0889105
Blaine Ehrhart <blaine@nextleveltiming.com>
2022-07-02 17:44:05 -0700

    feat: add serial example

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# neon-timing-serial-example
+# Neon Timing Serial Example
+
+This is an example of the [Neon Timing Protocol](https://github.com/NextLevelTiming/neon-timing-protocol) over a serial connection.
+
+This example will establish a connection to another Neon Timing client and wait for race events. Once race events are received the device will use the attached neopixels to display the current race state.
+
+- Race Stating: Solid red lights
+- Countdown Started: Animated flashing red lights
+- Countdown End Delay Started: Turns off all lights
+- Race Started: Solid green lights
+- Race Completed: Animated flashing white lights for 10 seconds, then red lights
+
+## Animated Light Shows
+It should be noted this code is more complex due to the animated light shows: countdown started and race completed. This is done to allow processing new Neon Timing Protocol messages while an these light shows are still being displayed.
+
+Most example code for NeoPixel animated lights will end up blocking the entire processor from doing anything during the animation. This example will intentionally interupt animations as we process new race events.
+
+## Components
+While this code will work for many ESP32 devices and can be ported to other types of microcontrollers- here are the components we tested with.
+
+> The 470 ohm resistor between the ESP32 and NeoPixels can help prevent damage to your first pixel.
+> Consider reading the [NeoPixel Uberguide](https://learn.adafruit.com/adafruit-neopixel-uberguide/basic-connections) to connect other types of NeoPixels.
+
+- [ESP32](https://www.adafruit.com/product/5325)
+- [NeoPixel Stick](https://www.adafruit.com/product/1426)
+- Connect board A0 -> ~470ohm resistor -> pixels DIN
+- Connect board V5-> pixels VDC
+- Connect board GND -> pixels GND
+
---
+++ src/main.cpp
@@ -0,0 +1,347 @@
+#include <ArduinoJson.h>
+#include <Adafruit_NeoPixel.h>
+#include <Preferences.h>
+
+// Preferences
+Preferences preferences;
+struct PreferencesStruct {
+  char deviceID[17] = "";
+};
+PreferencesStruct preferencesStorage;
+
+// Neon Timing Connection
+const int NT_PROTOCOL_DISCONNECTED = 0;
+const int NT_PROTOCOL_SERIAL = 1;
+int neonTimingConnectionProtocol = NT_PROTOCOL_DISCONNECTED;
+bool rncAllowedEventsLog = false;
+
+// Neon Timing Connection Light
+const int connectionLightToggleDelay = 1000;
+bool connectionLightEnabled = false;
+int connectionLightLastToggleTime = millis();
+
+// Serial
+const int SERIAL_BUFFER_MAX = 201;
+char serialBuffer[SERIAL_BUFFER_MAX];
+
+// Pixel Setup
+const int PIXEL_COUNT = 8;
+const int PIXEL_PIN = 18;
+Adafruit_NeoPixel pixelStrip = Adafruit_NeoPixel(PIXEL_COUNT, PIXEL_PIN, NEO_GRB + NEO_KHZ800);
+
+// Light show
+const int LS_NONE = 0;
+const int LS_COUNTDOWN_STARTED_ANIMATION = 1;
+const int LS_RACE_COMPLETED_ANIMATION = 2;
+int activeLightShow = LS_NONE;
+int lightShowStartTime = 0;
+int lightShowStateStartTime = 0;
+int lightShowState = -1;
+
+void setupPreferences();
+void setupPixels();
+void toggleConnectionLight(int toggle = -1);
+void startLightShow(int newLightShow);
+void stopLightShow();
+void onMessage(int communicationProtocol, JsonVariant messageDoc);
+void writeLog(const char* message);
+void serialMessageLoop();
+void sendHandshakeMessage(const char* type, int sendWithProtocol);
+void setAllPixels(byte red, byte green, byte blue);
+void setPixel(int pixel, byte red, byte green, byte blue);
+void lightShowLoop();
+void sendCommand(JsonDocument& doc, int sendWithProtocol = -1);
+void updateNeonTimingConnectionState(int newConnectionState);
+
+void setup() {
+
+  Serial.begin(115200);
+  Serial.println();
+
+  setupPreferences();
+  setupPixels();
+}
+
+void loop() {
+  serialMessageLoop();
+  lightShowLoop();
+}
+
+void setupPreferences() {
+
+  preferences.begin("neon-timing", false);
+  preferences.getString("device_id", preferencesStorage.deviceID, 17);
+  // If there isn't a device id saved into memory then store one
+  if (strcmp(preferencesStorage.deviceID, "") == 0) {
+    char did[10];
+    snprintf(did, 10, "%d", random(100000000, 999999999));
+    strncpy(preferencesStorage.deviceID, did, sizeof(did));
+    // Store the newly generated device id into storage, even on reboot this doesn't reset
+    preferences.putString("device_id", preferencesStorage.deviceID);
+    writeLog("Saved new Device ID");
+  }
+}
+
+void setupPixels() {
+
+  pixelStrip.begin();
+
+  // Initialize all pixels to 'off'
+  pixelStrip.show();
+}
+
+void toggleConnectionLight(int toggle) {
+
+  connectionLightLastToggleTime = millis();
+  if (toggle == 0) {
+    connectionLightEnabled = false;
+  }
+  else if (toggle > 0) {
+    connectionLightEnabled = true;
+  }
+  else {
+    connectionLightEnabled = !connectionLightEnabled;
+  }
+
+  if (connectionLightEnabled) {
+    setAllPixels(0, 0, 200); // Blue
+    pixelStrip.show();
+  } else {
+    setAllPixels(0, 0, 0); // Off
+    pixelStrip.show();
+  }
+}
+
+void startLightShow(int newLightShow) {
+
+  activeLightShow = newLightShow;
+  lightShowStartTime = millis();
+  lightShowState = -1;
+  lightShowStateStartTime = 0;
+
+  lightShowLoop();
+  return;
+}
+
+void stopLightShow() {
+  startLightShow(LS_NONE);
+}
+
+void onMessage(int communicationProtocol, JsonVariant messageDoc) {
+
+  if (!messageDoc.is<JsonObject>()) {
+    writeLog("Error processing message: message must be an object");
+    return;
+  }
+
+  if (!messageDoc["cmd"].is<const char*>()) {
+    writeLog("Error processing message: cmd must be a string");
+    return;
+  }
+  const char* command = messageDoc["cmd"];
+
+  if (neonTimingConnectionProtocol == NT_PROTOCOL_DISCONNECTED && strcmp(command, "handshake_init") != 0 && strcmp(command, "handshake_ack") != 0) {
+    writeLog("Error processing message: device not initialized");
+    return;
+  }
+
+  if (strcmp(command, "event") == 0) {
+    if (!messageDoc["evt"].is<const char*>()) {
+      writeLog("Error processing message: evt must be a string");
+      return;
+    }
+    if (!messageDoc["type"].is<const char*>()) {
+      writeLog("Error processing message: type must be a string");
+      return;
+    }
+    if (strcmp(messageDoc["evt"], "race") != 0 && strcmp(messageDoc["evt"], "flag") != 0) {
+      writeLog("Error processing message: evt value is not supported");
+      return;
+    }
+    const char* eventType = messageDoc["type"];
+    const char* evt = messageDoc["evt"];
+
+    if (strcmp(evt, "race") == 0) {
+      if (strcmp(eventType, "race_staging") == 0) {
+        stopLightShow();
+        setAllPixels(200, 0, 0); // Red
+      }
+      else if (strcmp(eventType, "countdown_started") == 0) {
+        startLightShow(LS_COUNTDOWN_STARTED_ANIMATION);
+      }
+      else if (strcmp(eventType, "countdown_end_delay_started") == 0) {
+        stopLightShow();
+        setAllPixels(0, 0, 0); // Off
+      }
+      else if (strcmp(eventType, "race_started") == 0) {
+        stopLightShow();
+        setAllPixels(0, 100, 0); // Green
+      }
+      else if (strcmp(eventType, "race_completed") == 0) {
+        startLightShow(LS_RACE_COMPLETED_ANIMATION);
+      }
+    }
+  }
+  else if (strcmp(command, "handshake_init") == 0) {
+    if (strcmp(messageDoc["protocol"].as<const char*>(), "NT1") != 0) {
+      writeLog("Error processing message: protocol must be NT1");
+      return;
+    }
+    if (!messageDoc["events"].is<JsonArray>()) {
+      writeLog("Error processing message: events must be an array");
+      return;
+    }
+
+    sendHandshakeMessage("handshake_ack", communicationProtocol);
+
+    for (JsonVariant value : messageDoc["events"].as<JsonArray>()) {
+      if (strcmp(value.as<const char*>(), "log") == 0) {
+        rncAllowedEventsLog = true;
+      }
+      else if (strcmp(value.as<const char*>(), "*") == 0) {
+        rncAllowedEventsLog = true;
+      }
+    }
+
+    if (neonTimingConnectionProtocol != communicationProtocol) {
+      updateNeonTimingConnectionState(communicationProtocol);
+      toggleConnectionLight(0);
+    }
+  }
+  else if (strcmp(command, "handshake_ack") == 0) {
+    if (strcmp(messageDoc["protocol"].as<const char*>(), "NT1") != 0) {
+        writeLog("Error processing message: protocol must be NT1");
+      return;
+    }
+  }
+  else {
+    StaticJsonDocument<200> cmd;
+    cmd["cmd"] = "event";
+    cmd["evt"] = "log";
+    cmd["message"] = "Invalid command";
+    cmd["data"]["command"] = command;
+    sendCommand(cmd);
+  }
+}
+
+void writeLog(const char* message) {
+
+  StaticJsonDocument<200> cmd;
+  cmd["cmd"] = "event";
+  cmd["evt"] = "log";
+  cmd["message"] = message;
+  sendCommand(cmd);
+}
+
+void serialMessageLoop() {
+
+  if (millis() - connectionLightLastToggleTime > connectionLightToggleDelay && neonTimingConnectionProtocol == NT_PROTOCOL_DISCONNECTED) {
+    toggleConnectionLight();
+  }
+
+  if (!Serial && neonTimingConnectionProtocol == NT_PROTOCOL_SERIAL) {
+    updateNeonTimingConnectionState(NT_PROTOCOL_DISCONNECTED);
+  }
+
+  if (Serial.available() <= 0) {
+    return;
+  }
+
+  Serial.readBytesUntil('\n', serialBuffer, SERIAL_BUFFER_MAX - 1);
+
+  StaticJsonDocument<512> messageDoc;
+  DeserializationError error = deserializeJson(messageDoc, serialBuffer);
+  if (error) {
+    writeLog("Error processing message: could not deserialize json");
+    return;
+  }
+
+  onMessage(NT_PROTOCOL_SERIAL, messageDoc);
+
+  // Reset buffer
+  memset(&serialBuffer, 0, SERIAL_BUFFER_MAX);
+}
+
+void sendHandshakeMessage(const char* type, int sendWithProtocol) {
+
+    StaticJsonDocument<200> cmd;
+    cmd["cmd"] = type;
+    cmd["device"] = "Race Lights";
+    JsonArray events = cmd.createNestedArray("events");
+    events.add("race");
+    sendCommand(cmd, sendWithProtocol);
+}
+
+void setAllPixels(byte red, byte green, byte blue) {
+
+  for(int i = 0; i < PIXEL_COUNT; i++) {
+    pixelStrip.setPixelColor(i, pixelStrip.Color(red, green, blue));
+  }
+  pixelStrip.show();
+}
+
+void setPixel(int pixel, byte red, byte green, byte blue) {
+
+  pixelStrip.setPixelColor(pixel, pixelStrip.Color(red, green, blue));
+  pixelStrip.show();
+}
+
+void lightShowLoop() {
+
+  if (activeLightShow == LS_NONE) {
+    return;
+  }
+
+  if (lightShowState < 0) {
+    lightShowState = 0;
+  }
+
+  if (activeLightShow == LS_COUNTDOWN_STARTED_ANIMATION) {
+    if (millis() - lightShowStateStartTime >= 500) {
+      if (lightShowState == 1) {
+        setAllPixels(0, 0, 0);
+        lightShowState = 0;
+      }
+      else {
+        setAllPixels(200, 0, 0);
+        lightShowState = 1;
+      }
+      lightShowStateStartTime = millis();
+    }
+  }
+  else if (activeLightShow == LS_RACE_COMPLETED_ANIMATION) {
+    if (millis() - lightShowStartTime >= 10000) {
+      setAllPixels(100, 0, 0); // Red
+      stopLightShow();
+      return;
+    }
+
+    if (millis() - lightShowStateStartTime >= 30) {
+      int pixel = random(PIXEL_COUNT);
+      setAllPixels(0, 0, 0);
+      setPixel(pixel, 255, 255, 255);
+      lightShowStateStartTime = millis();
+    }
+  }
+}
+
+void sendCommand(JsonDocument& doc, int sendWithProtocol) {
+  if (neonTimingConnectionProtocol != NT_PROTOCOL_DISCONNECTED && doc["evt"] == "log" && !rncAllowedEventsLog) {
+    return;
+  }
+
+  doc["protocol"] = "NT1";
+  doc["time"] = millis();
+  doc["did"] = preferencesStorage.deviceID;
+
+  serializeJson(doc, Serial);
+  Serial.println();
+}
+
+void updateNeonTimingConnectionState(int newConnectionState) {
+  neonTimingConnectionProtocol = newConnectionState;
+  if (newConnectionState == NT_PROTOCOL_DISCONNECTED) {
+    rncAllowedEventsLog = false;
+    startLightShow(LS_NONE);
+  }
+}
